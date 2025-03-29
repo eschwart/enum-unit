2c107db05966c1f8375bf58671bab2fa40d68bda
splurf <evandotschwartzentruber@gmail.com>
2025-03-29 14:14:05 -0400

    refac

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{Data, DeriveInput, Fields, parse_macro_input};
 
 #[proc_macro_derive(EnumUnit)]
 pub fn into_unit_enum(input: TokenStream) -> TokenStream {
@@ -64,6 +64,7 @@ pub fn into_unit_enum(input: TokenStream) -> TokenStream {
     );
 
     // basic implementation
+    #[cfg(not(feature = "bitflags"))]
     let new_enum = {
         let flag_arms = variants.iter().map(|variant| {
             let ident = &variant.ident;
@@ -79,97 +80,7 @@ pub fn into_unit_enum(input: TokenStream) -> TokenStream {
         }
     };
 
-    let doc_comment = format!("The [`{}`] of this [`{}`].", new_enum_name, old_enum_name);
-
-    // [`kind`] method and [`From`] trait implementation
-    let new_enum_impl = quote! {
-        impl #old_enum_name {
-            #[doc = #doc_comment]
-            pub const fn kind(&self) -> #new_enum_name {
-                match self {
-                    #(#match_arms)*
-                }
-            }
-        }
-
-        impl From<#old_enum_name> for #new_enum_name {
-            fn from(value: #old_enum_name) -> Self {
-                value.kind()
-            }
-        }
-    };
-
-    // putting it all together
-    quote! {
-        #new_enum
-        #new_enum_impl
-    }
-    .into()
-}
-
-#[cfg(feature = "bitflags")]
-#[proc_macro_derive(EnumUnitBit)]
-pub fn into_unit_enum_bf(input: TokenStream) -> TokenStream {
-    // parse the input tokens into a syntax tree
-    let input = parse_macro_input!(input as DeriveInput);
-
-    // check if the input is an enum
-    let variants = if let Data::Enum(data_enum) = input.data {
-        data_enum.variants
-    } else {
-        return quote! { compile_error!("Unsupported structure (enum's only)") }.into();
-    };
-
-    // return nothing if there are no variants
-    if variants.is_empty() {
-        return quote! {}.into();
-    }
-
-    // prepare names for each enumeration
-    let old_enum_name = input.ident;
-    let new_enum_name = quote::format_ident!("{}Unit", old_enum_name);
-
-    // obtain names of every variant
-    let match_arms = variants.iter().map(|variant| {
-        let ident = &variant.ident;
-
-        // Handle tuple and struct variants
-        match &variant.fields {
-            Fields::Unit => {
-                quote! {
-                    #old_enum_name::#ident => #new_enum_name::#ident,
-                }
-            }
-            Fields::Unnamed(_) => {
-                quote! {
-                    #old_enum_name::#ident(..) => #new_enum_name::#ident,
-                }
-            }
-            Fields::Named(_) => {
-                quote! {
-                    #old_enum_name::#ident { .. } => #new_enum_name::#ident,
-                }
-            }
-        }
-    });
-
-    // primitive derivations
-    let derive_inner = quote! {
-        Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord
-    };
-
-    // serde derivations
-    #[cfg(feature = "serde")]
-    let derive_inner = quote! {
-        #derive_inner, ::serde::Serialize, ::serde::Deserialize
-    };
-
-    let doc_comment = format!(
-        "Automatically generated unit-variants of [`{}`].",
-        old_enum_name
-    );
-
-    // bitflags implementation
+    #[cfg(feature = "bitflags")]
     let new_enum = {
         // size of the bitflag
         let size = match variants.len() {

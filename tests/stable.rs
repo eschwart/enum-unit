cc68b11ec45f960febda459b97701e7a9344f088
splurf <evandotschwartzentruber@gmail.com>
2025-03-29 14:14:32 -0400

    added & modulized tests

--- tests/stable.rs
+++ tests/stable.rs
@@ -17,19 +17,42 @@ fn test_stable() {
     assert_eq!(a.kind(), ExampleUnit::A);
     assert_eq!(b.kind(), ExampleUnit::B);
     assert_eq!(c.kind(), ExampleUnit::C);
+}
 
-    #[cfg(feature = "bitflag")]
-    {
-        let ab = ExampleUnit::A | ExampleUnit::B;
-        let ac = ExampleUnit::A | ExampleUnit::C;
-        let bc = ExampleUnit::B | ExampleUnit::C;
+#[test]
+#[cfg(feature = "bitflags")]
+fn test_bitflags() {
+    let ab = ExampleUnit::A | ExampleUnit::B;
+    let ac = ExampleUnit::A | ExampleUnit::C;
+    let bc = ExampleUnit::B | ExampleUnit::C;
 
-        let a = ab - ExampleUnit::B;
-        let b = bc - ExampleUnit::C;
-        let c = ac - ExampleUnit::A;
+    let a = ab - ExampleUnit::B;
+    let b = bc - ExampleUnit::C;
+    let c = ac - ExampleUnit::A;
 
-        assert_eq!(a, ExampleUnit::A);
-        assert_eq!(b, ExampleUnit::B);
-        assert_eq!(c, ExampleUnit::C)
-    }
+    assert_eq!(a, ExampleUnit::A);
+    assert_eq!(b, ExampleUnit::B);
+    assert_eq!(c, ExampleUnit::C)
+}
+
+#[test]
+#[cfg(feature = "serde")]
+fn test_serde() {
+    let a = Example::A.kind();
+
+    let serialized = serde_json::to_string(&a).unwrap();
+    let deserialized: ExampleUnit = serde_json::from_str(&serialized).unwrap();
+
+    assert_eq!(a, deserialized);
+}
+
+#[test]
+#[cfg(all(feature = "bitflags", feature = "serde"))]
+fn test_serde_bitflags() {
+    let ab = Example::A.kind() | Example::B(2).kind();
+
+    let ab_serialized = serde_json::to_string(&ab).unwrap();
+    let ab_deserialized: ExampleUnit = serde_json::from_str(&ab_serialized).unwrap();
+
+    assert_eq!(ab, ab_deserialized)
 }

c6070c959ade662cc37fcec57377ea7836f315fc
splurf <evandotschwartzentruber@gmail.com>
2025-05-06 12:16:21 -0400

    refac, impl tests for structs

--- tests/stable.rs
+++ tests/stable.rs
@@ -1,58 +1,123 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use enum_unit::*;
 
-#[allow(dead_code)]
+// === Types under test ===
+
 #[derive(EnumUnit)]
-enum Example {
+enum ExampleEnum {
     A,
     B(u8),
     C { id: &'static str },
 }
 
-#[test]
-fn test_stable() {
-    let a = Example::A;
-    let b = Example::B(69);
-    let c = Example::C { id: "Unit" };
-
-    assert_eq!(a.kind(), ExampleUnit::A);
-    assert_eq!(b.kind(), ExampleUnit::B);
-    assert_eq!(c.kind(), ExampleUnit::C);
+#[derive(EnumUnit)]
+struct ExampleNamedStruct {
+    a: (),
+    b: u8,
+    c: &'static str,
 }
 
-#[test]
-#[cfg(feature = "bitflags")]
-fn test_bitflags() {
-    let ab = ExampleUnit::A | ExampleUnit::B;
-    let ac = ExampleUnit::A | ExampleUnit::C;
-    let bc = ExampleUnit::B | ExampleUnit::C;
+// This is a valid derive but needs no test.
+#[derive(EnumUnit)]
+struct ExampleUnitStruct;
 
-    let a = ab - ExampleUnit::B;
-    let b = bc - ExampleUnit::C;
-    let c = ac - ExampleUnit::A;
+// === Test constants / constructors ===
 
-    assert_eq!(a, ExampleUnit::A);
-    assert_eq!(b, ExampleUnit::B);
-    assert_eq!(c, ExampleUnit::C)
+const EX_ENUM_A: ExampleEnum = ExampleEnum::A;
+const EX_ENUM_B: ExampleEnum = ExampleEnum::B(69);
+const EX_ENUM_C: ExampleEnum = ExampleEnum::C { id: "Unit" };
+
+// === Tests ===
+
+mod enum_tests {
+    use super::*;
+
+    #[test]
+    fn test_kind() {
+        assert_eq!(EX_ENUM_A.kind(), ExampleEnumUnit::A);
+        assert_eq!(EX_ENUM_B.kind(), ExampleEnumUnit::B);
+        assert_eq!(EX_ENUM_C.kind(), ExampleEnumUnit::C);
+    }
+
+    #[test]
+    #[cfg(feature = "bitflags")]
+    fn test_bitflags() {
+        let ab = ExampleEnumUnit::A | ExampleEnumUnit::B;
+        let ac = ExampleEnumUnit::A | ExampleEnumUnit::C;
+        let bc = ExampleEnumUnit::B | ExampleEnumUnit::C;
+
+        let a = ab - ExampleEnumUnit::B;
+        let b = bc - ExampleEnumUnit::C;
+        let c = ac - ExampleEnumUnit::A;
+
+        assert_eq!(a, ExampleEnumUnit::A);
+        assert_eq!(b, ExampleEnumUnit::B);
+        assert_eq!(c, ExampleEnumUnit::C);
+    }
+
+    #[test]
+    #[cfg(feature = "serde")]
+    fn test_serde() {
+        let a = EX_ENUM_A.kind();
+
+        let serialized = serde_json::to_string(&a).unwrap();
+        let deserialized: ExampleEnumUnit = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(a, deserialized);
+    }
+
+    #[test]
+    #[cfg(all(feature = "bitflags", feature = "serde"))]
+    fn test_serde_bitflags() {
+        let ab = EX_ENUM_A.kind() | EX_ENUM_B.kind();
+
+        let serialized = serde_json::to_string(&ab).unwrap();
+        let deserialized: ExampleEnumUnit = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(ab, deserialized);
+    }
 }
 
-#[test]
-#[cfg(feature = "serde")]
-fn test_serde() {
-    let a = Example::A.kind();
+mod struct_tests {
+    use super::*;
 
-    let serialized = serde_json::to_string(&a).unwrap();
-    let deserialized: ExampleUnit = serde_json::from_str(&serialized).unwrap();
+    #[test]
+    #[cfg(feature = "bitflags")]
+    fn test_bitflags() {
+        let ab = ExampleNamedStructUnit::A | ExampleNamedStructUnit::B;
+        let ac = ExampleNamedStructUnit::A | ExampleNamedStructUnit::C;
+        let bc = ExampleNamedStructUnit::B | ExampleNamedStructUnit::C;
 
-    assert_eq!(a, deserialized);
-}
+        let a = ab - ExampleNamedStructUnit::B;
+        let b = bc - ExampleNamedStructUnit::C;
+        let c = ac - ExampleNamedStructUnit::A;
 
-#[test]
-#[cfg(all(feature = "bitflags", feature = "serde"))]
-fn test_serde_bitflags() {
-    let ab = Example::A.kind() | Example::B(2).kind();
+        assert_eq!(a, ExampleNamedStructUnit::A);
+        assert_eq!(b, ExampleNamedStructUnit::B);
+        assert_eq!(c, ExampleNamedStructUnit::C);
+    }
 
-    let ab_serialized = serde_json::to_string(&ab).unwrap();
-    let ab_deserialized: ExampleUnit = serde_json::from_str(&ab_serialized).unwrap();
+    #[test]
+    #[cfg(feature = "serde")]
+    fn test_serde() {
+        let b = ExampleNamedStructUnit::B;
 
-    assert_eq!(ab, ab_deserialized)
+        let serialized = serde_json::to_string(&b).unwrap();
+        let deserialized: ExampleNamedStructUnit = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(b, deserialized);
+    }
+
+    #[test]
+    #[cfg(all(feature = "bitflags", feature = "serde"))]
+    fn test_serde_bitflags() {
+        let ab = ExampleNamedStructUnit::A | ExampleNamedStructUnit::B;
+
+        let serialized = serde_json::to_string(&ab).unwrap();
+        let deserialized: ExampleNamedStructUnit = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(ab, deserialized);
+    }
 }
